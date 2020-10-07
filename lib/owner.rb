4552f3df3fd5014b01ada2798c90f39e77ff633e
Christian Simon <csimon1127@gmail.com>
2020-10-07 23:49:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,6 @@
 class Owner
   attr_accessor :pets, :name, :species
+  attr_reader :species
 
   @@all = []
 
@@ -11,7 +12,7 @@ class Owner
   end
 
   def say_species
-    @species 
+    puts "I am a #{@species}" 
   end
 
   def self.all 
