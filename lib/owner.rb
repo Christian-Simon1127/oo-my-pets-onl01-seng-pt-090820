be58ee8e188b83a3a1ea060fd36801095f5b6c29
Christian Simon <csimon1127@gmail.com>
2020-10-07 23:38:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,15 +2,36 @@ class Owner
   attr_accessor :pets
   attr_reader :name, :species
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @species = human
     @pets = []
+    self << @@all
   end
 
   def say_species
     @species 
   end
 
+  def self.all 
+    @@all
+  end 
+
+  def self.count 
+    @@all.length
+  end 
+
+  def self.reset_all
+    @all.clear
+  end 
+  
+  
+  
+  
+  
+  
+  
 
 end
